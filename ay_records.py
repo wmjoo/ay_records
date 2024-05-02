30f2fba4e3f554a4d4b288cc6ebecb15431e26e8
wmjoo <38180565+wmjoo@users.noreply.github.com>
2024-05-03 02:09:23 +0900

    Update ay_records.py

--- ay_records.py
+++ ay_records.py
@@ -11,8 +11,11 @@ import requests
 import os
 import pickle
 from multiprocessing import Pool, cpu_count, Value, Lock
+import streamlit as st
 
-team_dict = {'레알루키즈': 'http://alb.or.kr//s/teams/index.php%3Fid%3Dteams_players%26league%3D%EC%97%B0%ED%95%A9%ED%9A%8C%EC%9E%A5%EA%B8%B0%26sc%3D2%26team%3D%EB%A0%88%EC%95%8C%EB%A3%A8%ED%82%A4%EC%A6%88%26order%3Dxb%26sort%3D%26gyear%3D2022',
+team_names_list = ['레알루키즈', '안양실버', '안양도깨비', '맥퀸즈클럽As', '천군', '과천토리즈', '해머스1999', '경기블루오션', '보이스캐디구디스', '안양동안경찰서', 'Slugger', '야르셀로나배트홀릭스']
+
+team_dict = {'레알루키즈': 'http://alb.or.kr/s/teams/index.php?id=teams_players&league=%BF%AC%C7%D5%C8%B8%C0%E5%B1%E2&sc=2&team=%B7%B9%BE%CB%B7%E7%C5%B0%C1%EE&order=xb&sort=',
  '안양실버': 'http://alb.or.kr/s/teams/index.php?id=teams_players&league=%BF%AC%C7%D5%C8%B8%C0%E5%B1%E2&sc=2&team=%BE%C8%BE%E7%BD%C7%B9%F6&order=xb&sort=&gyear=2024',
  '안양도깨비': 'http://alb.or.kr//s/teams/index.php%3Fid%3Dteams_players%26league%3D%EC%97%B0%ED%95%A9%ED%9A%8C%EC%9E%A5%EA%B8%B0%26sc%3D2%26team%3D%EC%95%88%EC%96%91%EB%8F%84%EA%B9%A8%EB%B9%84%26order%3Dxb%26sort%3D%26gyear%3D2022',
  '맥퀸즈클럽As': 'http://alb.or.kr//s/teams/index.php%3Fid%3Dteams_players%26league%3D%EC%97%B0%ED%95%A9%ED%9A%8C%EC%9E%A5%EA%B8%B0%26sc%3D2%26team%3D%EB%A7%A5%ED%80%B8%EC%A6%88%ED%81%B4%EB%9F%BDAs%26order%3Dxb%26sort%3D%26gyear%3D2022',
